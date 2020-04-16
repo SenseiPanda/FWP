33e24491eff0710052eb8ce4b05d8292d8609478
Daniel Lynch <45492286+SenseiPanda@users.noreply.github.com>
2020-04-16 17:34:34 -0400

    Add files via upload
    Original Project Files created by Greg Koch

---
+++ jscode.js
@@ -0,0 +1,6 @@
+var product1 = document.getElementById("mask");
+var prod = document.getElementById("mask");
+var clicked = function() {
+  alert("thank you for clicking");
+  prod.style.display = "block";
+}
