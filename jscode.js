d41650fcd1e1a8710bb4a16ee6a6d6e543f333ae
gregk89 <63820242+gregk89@users.noreply.github.com>
2020-04-16 19:39:12 -0400

    Add files via upload

--- jscode.js
+++ jscode.js
@@ -1,6 +1,13 @@
-var product1 = document.getElementById("mask");
-var prod = document.getElementById("mask");
-var clicked = function() {
-  alert("thank you for clicking");
-  prod.style.display = "block";
+
+
+//This function provides the actions after a user clicks a product
+//to puchase and add to cart
+var purchase = function() {
+
+  //open up a window after they click
+  alert("This item has been added to your cart");
+
+  //shows the cart after a click of a product
+  document.getElementById("aside").style.display = "block";
+
 }
